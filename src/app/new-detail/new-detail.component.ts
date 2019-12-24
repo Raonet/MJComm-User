18f4ca3df8e21ab6a8b8329f2aeab4d91710b069
Raonet <1027513492@qq.com>
2019-12-24 11:40:54 +0800

    主页新闻可以跳转至详细页面

--- src/app/new-detail/new-detail.component.ts
+++ src/app/new-detail/new-detail.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DefaultmsgService } from '../defaultmsg.service';
 
 @Component({
   selector: 'app-new-detail',
   templateUrl: './new-detail.component.html',
   styleUrls: ['./new-detail.component.css']
 })
+
 export class NewDetailComponent implements OnInit {
 
-  constructor(private routerinfo: ActivatedRoute) { }
+  constructor(private routerinfo: ActivatedRoute, private dehttpService: DefaultmsgService) { }
   id;
+  news = {
+    title: '',
+    _id: '',
+    content: ''
+  };
   ngOnInit() {
+    this.id = this.routerinfo.snapshot.queryParams.id;
+    console.log(this.id);
+    this.getNews();
   }
-
+  async getNews() {
+    let data;
+    await this.dehttpService.getNews().then(res => {data = res; });
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < data.length; i++) {
+      if (data[i]._id === this.id) {
+        this.news = data[i];
+        return ;
+      }
+    }
+  }
 }
