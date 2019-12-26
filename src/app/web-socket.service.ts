e86b27c4274c62bde9d30c715aeaf725a92c3a9e
Raonet <1027513492@qq.com>
2019-12-26 15:06:08 +0800

    修复论坛图片显示问题，增加聊天室连接提示

--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,20 @@ import { Subject } from 'rxjs';
 export class WebSocketService {
   private ws: any;
   private message: Subject<any> = new Subject<any>();
-  constructor() { }
+  constructor(private msg: NzMessageService) { }
   connectSocket(url) {
     this.ws = io(url);
     this.ws.on('connect_error', (error) => {
-      console.log('connect_error');
+      this.msg.info('连接聊天服务器失败，请检测网络连接！');
     });
     this.ws.on('reconnecting', (timeout) => {
       if (timeout === 3) {
       this.ws.close();
+      this.msg.info('连接聊天服务器失败，请检测网络连接！');
       }
+    });
+    this.ws.on('connect', (data) => {
+      this.msg.info('连接聊天服务器成功！');
     });
     this.ws.on('events', (data) => {
       this.analysisMessage(data);
